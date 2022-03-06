5e5afb937f3d4f5ff2b17384433e5955cf7f1cf9
Clément Causer <ccauser@simplonprod.co>
2022-03-06 12:40:44 +0100

    🔧  Update Firebase config and initialization

--- firebase/client.ts
+++ firebase/client.ts
@@ -1,20 +1,26 @@
-import firebase from "firebase/app";
+import firebase, { FirebaseOptions, getApp, initializeApp } from "firebase/app";
 import "firebase/auth";
+import { getAuth } from "firebase/auth";
 import "firebase/firestore";
 
-const credentials = {
+const options: FirebaseOptions = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
   authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
   projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
   storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_APP_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 
-// check if a firebase instance (app) is already up
-if (!firebase.getApps().length) {
-  // if not, then creates one
-  firebase.initializeApp(credentials);
-}
+const createFirebaseApp = (config: FirebaseOptions) => {
+  try {
+    return getApp();
+  } catch {
+    return initializeApp(config);
+  }
+};
+
+const firebaseApp = createFirebaseApp(options);
+export const firebaseAuth = getAuth(firebaseApp);
 
 export default firebase;
