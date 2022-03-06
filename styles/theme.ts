eb0952f5a18444e8c3d7397da46af1775277de5c
Clément Causer <ccauser@simplonprod.co>
2022-03-06 23:54:24 +0100

    💄 Update theme

--- styles/theme.ts
+++ styles/theme.ts
@@ -1,11 +1,20 @@
 import { createTheme, responsiveFontSizes } from "@mui/material/styles";
-import { deepPurple, amber } from "@mui/material/colors";
 
 // Create a theme instance
 let theme = createTheme({
   palette: {
-    primary: deepPurple,
-    secondary: amber,
+    primary: {
+      light: "#757ce8",
+      main: "#3f50b5",
+      dark: "#002884",
+      contrastText: "#fff",
+    },
+    secondary: {
+      light: "#ff7961",
+      main: "#f44336",
+      dark: "#ba000d",
+      contrastText: "#000",
+    },
   },
 });
 
