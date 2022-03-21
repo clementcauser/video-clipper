a293d42289b54440ff507fb5ada41ed48b1a8791
Clément Causer <ccauser@simplonprod.co>
2022-03-21 12:13:44 +0100

    🚨 Fix linter

--- utils/auth/getUserFromCookie.ts
+++ utils/auth/getUserFromCookie.ts
@@ -1,12 +1,9 @@
 import { firebaseCookieName } from "@constants";
 import { verifyTokenId } from "@firebase/admin";
-import { DecodedIdToken } from "firebase-admin/lib/auth/token-verifier";
 import { GetServerSidePropsContext } from "next";
 
 export const getUserFromCookie = async (ctx: GetServerSidePropsContext) => {
-  const user: DecodedIdToken = await verifyTokenId(
-    ctx.req.cookies[firebaseCookieName]
-  );
+  const user = await verifyTokenId(ctx.req.cookies[firebaseCookieName]);
 
   return user;
 };
